24b58f1f6a361b602b9e379d5691a35a45223410
Alexey Petrovskiy <alexdreamer@mail.ru>
2023-04-15 19:33:02 +0800

    Fixed locals.tf

--- terraform04/src/locals.tf
+++ terraform04/src/locals.tf
@@ -1,3 +1,4 @@
 locals {
-  path_to_ssh = file("~/.ssh/id_rsa.pub")
+  #path_to_ssh = file("~/.ssh/id_rsa.pub")
+  path_to_ssh = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSGqDtsF12EhSz1uout3DB3R69mSr3BAmCwlAIP59oJSEIULQdTok93MfZPtjQ6VculDGwIYachGBjD5y25j20uypdimFZj/dy/x9rPobiEPo8YyNn9ib89xamx4JwDbHgphbJ+liwBskj0gBb+BKltw3KHQEhKce4Vvntrf1pjPVFSiO2PVJRRVlqeyDmtLuYeTvfmSqU/sWYvam8tEnGD/0JmWQWyUz6y/+aFkn/iXQpXu+F/G1HgD3b9D+gTaSlv2/AykllBuwFJYQd6vwAmzJtHxSTmJMjvZM5elzw3gFF80f/VBol8epfjU5KyK3DYvnqglrOv6MxKhODKmI9 alex@ex"
 }
