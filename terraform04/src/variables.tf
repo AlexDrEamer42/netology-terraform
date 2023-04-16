b5eb7a703984e0a1b2f714f90ccddfebcdc1f4ae
Alexey Petrovskiy <alexdreamer@mail.ru>
2023-04-16 16:44:29 +0800

    Fixed variables.tf

--- terraform04/src/variables.tf
+++ terraform04/src/variables.tf
@@ -7,11 +7,13 @@ variable "token" {
 variable "cloud_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+  default     = "b1g1nmcaed0kbughlnoh"
 }
 
 variable "folder_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+  default     = "b1g8ggid6dd56um6f1t1"
 }
 
 variable "default_zone" {
