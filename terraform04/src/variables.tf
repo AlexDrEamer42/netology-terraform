f1ccdbe0623a3e57b6f8b1a74cbed4c59b5e5506
Alexey Petrovskiy <alexdreamer@mail.ru>
2023-04-16 17:15:22 +0800

    Return settings

--- terraform04/src/variables.tf
+++ terraform04/src/variables.tf
@@ -7,13 +7,11 @@ variable "token" {
 variable "cloud_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
-  default     = "b1g1nmcaed0kbughlnoh"
 }
 
 variable "folder_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
-  default     = "b1g8ggid6dd56um6f1t1"
 }
 
 variable "default_zone" {
